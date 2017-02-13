c48c56d56107fccd8970ce71a8bcdcf71ef2e8fa
Ralf Vogler <ralf.vogler@gmail.com>
2017-02-13 10:36:19 +0100

    use signedInDevices (dev channel only) to avoid closing on current device

--- background.js
+++ background.js
@@ -1,8 +1,28 @@
+var thisDevice;
+chrome.signedInDevices.get(true, function (x) {
+	thisDevice = x[0].name;
+});
+
 chrome.storage.onChanged.addListener(function (changes, namespace) {
 	for (key in changes) {
 		var x = changes[key];
-		console.log('Storage key', key, 'in namespace', namespace, 'changed from', x.oldValue, 'to', x.newValue);
+		// console.log('Storage key', key, 'in namespace', namespace, 'changed from', x.oldValue, 'to', x.newValue);
 		// TODO apparently there's no way to find out the current device name
 		// https://developer.chrome.com/extensions/signedInDevices
+		var close = x.newValue;
+		console.log(thisDevice);
+		for (device in close) {
+			console.log(device, close);
+			if (device == thisDevice && close[device].length) {
+				console.log("should close on this device");
+				chrome.tabs.query({ "url": close[device] }, function (tabs) {
+					console.log("should close the tabs", tabs);
+					chrome.tabs.remove(tabs.map(x => x.id));
+				});
+				close[device] = [];
+				chrome.storage.sync.set({ "close": close });
+				break;				
+			}
+		}
 	}
 });
